28d5b1a90996b668e4df522179d1b5f1ce6462e8
Anriu Oliveira <anriu.oliveira12@gmail.com>
2022-05-16 19:55:24 -0300

    Inicio do Projeto

---
+++ src/program/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package program;
+
+/**
+ *
+ * @author kiuar
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+}
